b5e7f026cc5c574b1b7a6ceb3915b1f9d954650e
irohitpanchal <67911705+irohitpanchal@users.noreply.github.com>
2020-07-06 22:52:20 +0530

    loader animation

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+
+
 /* Open */
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
