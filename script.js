d48334fe616095014981abacd43f27e6f03b6111
irohitpanchal <67911705+irohitpanchal@users.noreply.github.com>
2020-07-11 01:02:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,54 @@
-var myVar;
 
-function myFunction() {
-  myVar = setTimeout(showPage, 1000);
-}
+    var myVar;
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+    function myFunction() {
+        myVar = setTimeout(showPage, 1000);
+    }
 
-
-
-/* Open */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
+    function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+    }
 
 
+    /* Open */
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
 
-//smooth scroll
-// Select all links with hashes
-$('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-        // On-page links
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-            location.hostname == this.hostname
-        ) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000, function () {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
-                });
-            }
-        }
+    /* Close */
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+    function smoothscroll() {
+
+    }
+
+$(document).ready(function () {
+    
+
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
+});
